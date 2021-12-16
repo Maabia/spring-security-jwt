0772b501a2c3ed3b548cda0dc6af2400c8a74457
Maabia <maabia.ferry16@gmail.com>
2021-12-16 13:28:49 -0300

    class rolemodel implementada

--- src/main/java/com/example/jwt/model/UserModel.java
+++ src/main/java/com/example/jwt/model/UserModel.java
@@ -1,10 +1,22 @@
 package com.example.jwt.model;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@Table(name = "user")
 public class UserModel {
 	
 	@Id
@@ -19,4 +31,6 @@ public class UserModel {
 	
 	@Column(name = "creaded_at")
 	private LocalDateTime createdAt;
+	
+	private List<RoleModel> roles;
 }
