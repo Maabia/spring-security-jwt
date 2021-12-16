0f5154a8e2830a49efab129a192c27597a4f528e
Maabia <maabia.ferry16@gmail.com>
2021-12-16 16:25:34 -0300

    bloco de código criado apenas para facilitar e o foco ser no spring security

--- src/main/java/com/example/jwt/ApiRestSpringSecurityJwtApplication.java
+++ src/main/java/com/example/jwt/ApiRestSpringSecurityJwtApplication.java
@@ -1,7 +1,15 @@
 package com.example.jwt;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.example.jwt.model.RoleModel;
+import com.example.jwt.model.UserModel;
+import com.example.jwt.repository.RoleRepository;
+import com.example.jwt.service.UserService;
 
 @SpringBootApplication
 public class ApiRestSpringSecurityJwtApplication {
@@ -9,5 +17,28 @@ public class ApiRestSpringSecurityJwtApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApiRestSpringSecurityJwtApplication.class, args);
 	}
+	
+	// APENAS PARA NÃO TER QUE TODA HORA FICAR CADASTRANDO 
+	 @Bean
+	    public BCryptPasswordEncoder passwordEncoder() {
+	        return new BCryptPasswordEncoder();
+	    }
+
+	    @Bean
+	    CommandLineRunner runner(RoleRepository roleRepository, UserService userService) {
+	        return args -> {
+	            roleRepository.save(new RoleModel("ROLE_MASTER"));
+	            roleRepository.save(new RoleModel("ROLE_ADMIN"));
+	            roleRepository.save(new RoleModel("ROLE_USER"));
+
+	            userService.save(new UserModel("Pedro", "pedro@gmail.com", "123456"));
+	            userService.save(new UserModel("Fuskinha", "fuskinha@gmail.com", "123456"));
+	            userService.save(new UserModel("Ana", "ana@gmail.com", "123456"));
+
+	            userService.addRoleToUser("pedro@gmail.com", "ROLE_ADMIN");
+	            userService.addRoleToUser("fuskinha@gmail.com", "ROLE_MASTER");
+	            userService.addRoleToUser("ana@gmail.com", "ROLE_USER");
+	        };
+	    }
 
 }
