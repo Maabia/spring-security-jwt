28db66632d260eac582c2cfc36ae0bedbf0adfc1
Maabia <maabia.ferry16@gmail.com>
2021-12-16 17:35:18 -0300

    securtyconfig em andamento

--- src/main/java/com/example/jwt/security/SecurityConfig.java
+++ src/main/java/com/example/jwt/security/SecurityConfig.java
@@ -1,10 +1,12 @@
 package com.example.jwt.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
@@ -23,8 +25,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		auth.userDetailsService(this.userDetailsService).passwordEncoder(this.bCryptPasswordEncoder);
 	}
 	
+	// É AQUI QUE CONTROLO QUAIS RECURSOS PODEM SER ACESSADOS ATRAVÉS DO TIPO DE PERFIL DO USUÁRIO
 	protected void configure(HttpSecurity http) throws Exception {
-		super.configure(http);
+		http.csrf().disable();
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		http.authorizeRequests().antMatchers("/roles").hasAuthority("ROLE_MASTER");
+		http.authorizeRequests().antMatchers(HttpMethod.POST, "/users").hasAnyAuthority("ROLE_MASTER", "ROLE_ADMIN");
+		http.authorizeRequests().antMatchers(HttpMethod.GET, "/users").hasAnyAuthority("ROLE_MASTER", "ROLE_ADMIN", "ROLE_USER");
+		http.authorizeRequests().anyRequest().authenticated();
 	}
 	
 }
