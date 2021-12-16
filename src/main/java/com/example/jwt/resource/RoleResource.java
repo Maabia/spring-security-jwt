9bf46f08c9f0e5f27611af004c2b31fea0b5a578
Maabia <maabia.ferry16@gmail.com>
2021-12-16 16:20:41 -0300

    recurso role criado

---
+++ src/main/java/com/example/jwt/resource/RoleResource.java
@@ -0,0 +1,28 @@
+package com.example.jwt.resource;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.jwt.model.RoleModel;
+import com.example.jwt.repository.RoleRepository;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/roles")
+public class RoleResource {
+	
+	private RoleRepository roleRepository;
+	
+	@GetMapping
+	public List<RoleModel> findAll() { return this.roleRepository.findAll(); }
+	
+	@PostMapping
+	public RoleModel save(@RequestBody RoleModel roleModel) { return this.roleRepository.save(roleModel); }
+}
