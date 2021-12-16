9e697a035d1e33676bc389504a91a875244ce864
Maabia <maabia.ferry16@gmail.com>
2021-12-16 15:37:00 -0300

    tipo do id corrigido para Long

--- src/main/java/com/example/jwt/repository/RoleRepository.java
+++ src/main/java/com/example/jwt/repository/RoleRepository.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 import com.example.jwt.model.RoleModel;
 
 @Repository
-public interface RoleRepository extends JpaRepository<RoleModel, String> {
+public interface RoleRepository extends JpaRepository<RoleModel, Long> {
 	
 	Optional<RoleModel> findByName(String name);
 }
