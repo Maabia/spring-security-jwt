c1bd59f33d94e91236da50ca90d02da5567ddcbe
Maabia <maabia.ferry16@gmail.com>
2021-12-16 13:20:19 -0300

    class user criada e mapeada e class Role model criado

--- src/main/java/com/example/jwt/model/UserModel.java
+++ src/main/java/com/example/jwt/model/UserModel.java
@@ -1,5 +1,22 @@
 package com.example.jwt.model;
 
-public class UserModel {
+import java.time.LocalDateTime;
 
+import javax.persistence.Column;
+import javax.persistence.Id;
+
+public class UserModel {
+	
+	@Id
+	private String id;
+	
+	private String name;
+	
+	@Column(unique = true)
+	private String email;
+	
+	private String password;
+	
+	@Column(name = "creaded_at")
+	private LocalDateTime createdAt;
 }
